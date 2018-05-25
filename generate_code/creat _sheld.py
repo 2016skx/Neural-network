ae0e86433a384cd260594d22168923fb1ea51b4b
2016skx <39268504+2016skx@users.noreply.github.com>
2018-05-25 13:16:00 +0800

    Update creat _sheld.py

--- generate_code/creat _sheld.py
+++ generate_code/creat _sheld.py
@@ -7,11 +7,8 @@ class ltm:
 
     def __init__(self):
         self.dir_name = "input"
-        self.file_name = "11.txt"
-        self.totalNum=34
-        self.trainNum=10000
-        self.shel=0.2
-        self.NODE_THRESHOLD = 12
+        self.file_name = "11.txt"       #data set
+        self.totalNum=34                    #node num
         self.edge_to_weight_dict = {}
         self.node_to_incoming_edge_dict = {}
         self.node_to_activation_count_dict = {}
@@ -38,11 +35,11 @@ class ltm:
         self.fd.close()
 
     def pre_processing(self):
-        f1 = open('xxxxxxxxxxxxxxxxx', 'w', encoding='utf-8')              #Generate a threshold for each node
+        f1 = open('xxxxxxxxxxxxxxxxx.txt', 'w', encoding='utf-8')              #Generate a threshold for each node
         for node in self.nodes_list:
             if not node:
                 continue
-            nodesheld = round(random.uniform(0, 1),2)
+            nodesheld = round(random.uniform(0, 1),2)             #random from (0,1)
             f1.write(node)
             f1.write(' ')
             f1.write(str(nodesheld))
